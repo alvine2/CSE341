cbfa970b6389f1956fd3f64466fa8ea6dc459ad7
alvine2 <alvinekinyera@gmail.com>
2025-12-01 13:51:14 +0300

    CRUD

--- routes/appointments.js
+++ routes/appointments.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
-const ordersController = require('../controllers/appointments');
-const { orderValidationRules, validate } = require('../middleware/validate');
+const appointmentsController = require('../controllers/appointments');
+const { appointmentValidationRules, validate } = require('../middleware/validate'); 
 const { isAuthenticated } = require('../middleware/authenticate');
 
 /* CRUD routes */
 router.get('/', appointmentsController.getAll);
 router.get('/:id', appointmentsController.getSingle);
-router.post('/', isAuthenticated, appointmentValidationRules, validate, appointmentsController.createAppointment);
-router.put('/:id', isAuthenticated, appointmentValidationRules, validate, appointmentsController.updateAppointment);
+router.post('/', isAuthenticated,  appointmentsController.createAppointment);
+router.put('/:id', isAuthenticated,  appointmentsController.updateAppointment); 
 router.delete('/:id', isAuthenticated, appointmentsController.deleteAppointment);
+
 module.exports = router;
